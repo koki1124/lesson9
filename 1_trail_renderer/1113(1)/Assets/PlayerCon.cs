bdf0b35b4a85224e4a133b5e8758d656c0e6bd8a
kuro-tt <koki11241124@gmail.com>
2019-11-13 18:48:19 +0900

    Add
    自分でポリゴン描画まで

---
+++ 1_trail_renderer/1113(1)/Assets/PlayerCon.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    GameObject sword;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sword = transform.GetChild(0).gameObject;
+    }
+        // Update is called once per frame
+        void Update()
+        {
+            // 左に移動
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.transform.Translate(-0.1f, 0.0f, 0.0f);
+            }
+            // 右に移動
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.transform.Translate(0.1f, 0.0f, 0.0f);
+            }
+            // 前に移動
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                this.transform.Translate(0.0f, 0.0f, 0.1f);
+            }
+            // 後ろに移動
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                this.transform.Translate(0.0f, 0.0f, -0.1f);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 5, 0));
+        }
+
+        }
+
+        void Swipe()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Vector3 target_pos = ray.GetPoint(5.0f);
+
+                sword.transform.LookAt(target_pos);
+
+            }
+        }
+    }
---
+++ 1_trail_renderer/1113(1)/Assets/TrailController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
+public class TrailController : MonoBehaviour
+{
+    private const int FRAME_MAX = 10;
+    private List<Vector3> Point0 = new List<Vector3>();
+    private List<Vector3> Point1 = new List<Vector3>();
+
+    private Mesh mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FRAME_MAX <= Point0.Count)
+        {
+            Point0.RemoveAt(0);
+            Point1.RemoveAt(0);
+        }
+
+        Point0.Add(transform.position);
+        Point1.Add(transform.TransformPoint(new Vector3(0.0f, 1.0f, 0.0f)));
+
+        if (2 <= Point0.Count)
+        {
+            CreateMesh();
+        }
+       
+    }
+
+    private void CreateMesh()
+    {
+        mesh.Clear();
+
+        int n = Point0.Count;
+        Vector3[] vertexArray = new Vector3[2 * n];
+        Vector2[] uvArray = new Vector2[2 * n];
+        int[] inderArray = new int[6 * (n - 1)];
+
+        int idV = 0, idI = 0;
+        float dUv = 1.0f / ((float)n - 1.0f);
+        for (int i = 0; i < n; i++)
+        {
+            //座標頂点
+            vertexArray[idV + 0] = transform.InverseTransformPoint(Point0[i]);
+            vertexArray[idV + 1] = transform.InverseTransformPoint(Point1[i]);
+
+            //UV座標
+            uvArray[idV + 0].x =
+            uvArray[idV + 1].x = 1.0f - dUv * (float)i;
+            uvArray[idV + 0].y = 0.0f;
+            uvArray[idV + 1].y = 1.0f;
+
+            //インデックス
+            if (i == n - 1) break;
+            inderArray[idI + 0] = idV;
+            inderArray[idI + 1] = idV + 1;
+            inderArray[idI + 2] = idV + 2;
+            inderArray[idI + 3] = idV + 2;
+            inderArray[idI + 4] = idV + 1;
+            inderArray[idI + 5] = idV + 3;
+
+            idV += 2;
+            idI += 6;
+        }
+
+        mesh.vertices = vertexArray;
+        mesh.uv = uvArray;
+        mesh.triangles = inderArray;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        }
+    }
+
+
